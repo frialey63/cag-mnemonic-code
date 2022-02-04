6d2eb2550891eb45cfb32e03ba1abc39e93070cb
frialey63 <frialey63@googlemail.com>
2022-02-04 15:57:24 +0000

    junit testing for the order and arity exceptions

--- src/test/java/org/pjp/cag/OrderTest.java
+++ src/test/java/org/pjp/cag/OrderTest.java
@@ -5,11 +5,42 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.pjp.cag.exception.IncorrectArityException;
+import org.pjp.cag.exception.UnknownOrderException;
 
 public class OrderTest {
 
     /*
      * Order without address
+     */
+
+    @Test
+    public void testCreateJST() {
+        Order order = Order.create(false, "JST", null, null);
+
+        assertFalse(order.query);
+        assertEquals(OrderNumber.JST, order.orderNumber);
+        assertFalse(order.hasAddress());
+        assertFalse(order.hasModifier());
+    }
+
+    @Test
+    public void testCreateQJST() {
+        Order order = Order.create(true, "JST", null, null);
+
+        assertTrue(order.query);
+        assertEquals(OrderNumber.JST, order.orderNumber);
+        assertFalse(order.hasAddress());
+        assertFalse(order.hasModifier());
+    }
+
+    @Test(expected = IncorrectArityException.class)
+    public void testCreateJSTTooManyArgs() {
+        Order.create(false, "JST", "666", null);
+    }
+
+    /*
+     * Order with address
      */
 
     @Test
@@ -32,38 +63,33 @@ public class OrderTest {
         assertFalse(order.hasModifier());
     }
 
-    /*
-     * Order with address
-     */
-
-    @Test
-    public void testCreateLDAN() {
-        Order order = Order.create(false, "LDAN", "123", null);
-
-        assertFalse(order.query);
-        assertEquals(OrderNumber.LDAN, order.orderNumber);
-        assertTrue(order.hasAddress());
-        assertEquals(123, order.address);
-        assertFalse(order.hasModifier());
+    @Test(expected = IncorrectArityException.class)
+    public void testCreateARCMissingArg() {
+        Order.create(false, "ARC", null, null);
     }
 
-    @Test
-    public void testCreateQLDAN() {
-        Order order = Order.create(true, "LDAN", "123", null);
-
-        assertTrue(order.query);
-        assertEquals(OrderNumber.LDAN, order.orderNumber);
-        assertTrue(order.hasAddress());
-        assertEquals(123, order.address);
-        assertFalse(order.hasModifier());
+    @Test(expected = IncorrectArityException.class)
+    public void testCreateARCMissingTooManyArgs() {
+        Order.create(false, "ARC", "999", "9");
     }
 
     /*
-     * Order with address and modifier
+     * Order with address and (optional) modifier
      */
 
     @Test
     public void testCreateLDA() {
+        Order order = Order.create(false, "LDA", "123", null);
+
+        assertFalse(order.query);
+        assertEquals(OrderNumber.LDA, order.orderNumber);
+        assertTrue(order.hasAddress());
+        assertFalse(order.hasModifier());
+        assertEquals(123, order.address);
+    }
+
+    @Test
+    public void testCreateLDAModified() {
         Order order = Order.create(false, "LDA", "123", "3");
 
         assertFalse(order.query);
@@ -76,6 +102,17 @@ public class OrderTest {
 
     @Test
     public void testCreateQLDA() {
+        Order order = Order.create(true, "LDA", "123", null);
+
+        assertTrue(order.query);
+        assertEquals(OrderNumber.LDA, order.orderNumber);
+        assertTrue(order.hasAddress());
+        assertFalse(order.hasModifier());
+        assertEquals(123, order.address);
+    }
+
+    @Test
+    public void testCreateQLDAModified() {
         Order order = Order.create(true, "LDA", "123", "3");
 
         assertTrue(order.query);
@@ -85,5 +122,19 @@ public class OrderTest {
         assertEquals(123, order.address);
         assertEquals(3, order.modifier);
     }
+
+    @Test(expected = IncorrectArityException.class)
+    public void testCreateLDAMissingArg() {
+        Order.create(false, "LDA", null, null);
+    }
+
+    /*
+     * Unknown Order
+     */
+    @Test(expected = UnknownOrderException.class)
+    public void testCreateUnknown() {
+        Order.create(false, "ABC", null, null);
+    }
+
 
 }
