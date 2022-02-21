41444779b9934f708e96e247db3fcdb3b7283c28
frialey63 <frialey63@googlemail.com>
2022-02-21 22:24:58 +0000

    added TODO marker

--- src/main/java/org/pjp/cag/cpu/package-info.java
+++ src/main/java/org/pjp/cag/cpu/package-info.java
@@ -1,4 +1,5 @@
 /**
  * This package contains the CPU for the CAG Mnemonic Code.
+ * TODO introduce the CPU object which contains the accumulator, control register and store
  */
 package org.pjp.cag.cpu;
